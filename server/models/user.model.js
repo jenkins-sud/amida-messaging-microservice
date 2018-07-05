d26857879159414f37d4d45305c719c75185c1d5
Elijah George <elijah@amida.com>
2018-07-05 14:25:56 -0400

    remove unused instance methods

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -16,26 +16,6 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         }
     });
-    User.prototype.sendPushNotification = function(pushData) {
-      const { username } = this;
-      const authArgs = {
-          headers: {"Content-Type": "application/json"},
-          data: {
-            username: config.microserviceAccessKey,
-            password: config.microservicePassword
-          }
-      };
-      client.post(`${config.authMicroService}/v0/auth/login`, authArgs, function (data, response) {
-          const { token } = data;
-          const pushNotificationArgs = {
-              headers: {"Content-Type": "application/json", "Authorization":"Bearer " + token},
-              data: {...pushData, username},
-          };
-          client.post(`${config.notificationMicroservice}/users/sendPushNotification`, pushNotificationArgs, function (data, response) {
-          });
-      });
-    };
     // Class methods
-
     return User;
 };
